21731e13ee5eb4795553d1b458f8da3e34470493
Neil Doshi <mail.neildoshi@gmail.com>
2024-03-01 17:47:45 +0530

    add create operation with gorm

--- main.go
+++ main.go
@@ -1,25 +1,36 @@
 package main
 
 import (
+	"fmt"
+	"go-api/src/models"
+	"go-api/src/utils"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
+	utils.LoadEnv()
+	db := models.OpenDatabaseConnection()
+
+	newUser := models.User{
+		FirstName: "Jane",
+		LastName:  "Doe",
+		Email:     "jane.doe@gmail.com",
+		Country:   "Spain",
+		Age:       25,
+	}
+
+	result := db.Create(&newUser)
+	if result.Error != nil {
+		panic("failed to create a new user" + result.Error.Error())
+	}
+
+	fmt.Printf("New User added successfully")
 
 	router := gin.Default()
 	router.GET("/gin", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{"message": "Hello World"})
 	})
 	router.Run(":8080")
-
-	// newUser := User{
-	// 	FirstName: "Jane",
-	// 	LastName:  "Doe",
-	// 	Email:     "jane.doe@gmail.com",
-	// 	Country:   "Spain",
-	// 	Age:       25,
-	// }
-
 }
--- src/models/db.go
+++ src/models/db.go
@@ -13,24 +13,22 @@ var Database *gorm.DB
 func OpenDatabaseConnection() *gorm.DB {
 
 	host := os.Getenv("POSTGRES_HOST")
-	username := os.Getenv("POSTGRES_USERNAME")
+	username := os.Getenv("POSTGRES_USER")
 	password := os.Getenv("POSTGRES_PASSWORD")
 	database := os.Getenv("POSTGRES_DATABASE")
 	port := os.Getenv("POSTGRES_PORT")
 
-	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=required TimeZone=Asia/Kolkata", host, username, password, database, port)
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s", host, username, password, database, port)
 
 	Database, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		panic("failed to connect to database" + err.Error())
 	}
 
-	return Database
-}
-
-func AutoMigrationModels() {
-	err := Database.AutoMigrate(&User{})
-	if err != nil {
+	migrationErr := Database.AutoMigrate(&User{})
+	if migrationErr != nil {
 		panic("failed to perform database migration")
 	}
+
+	return Database
 }
