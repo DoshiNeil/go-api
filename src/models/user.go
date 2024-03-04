d9187081d19cda9535b72796c731b47e07f9959d
Neil Doshi <mail.neildoshi@gmail.com>
2024-03-04 16:06:35 +0530

    add json shape to user model

--- src/models/user.go
+++ src/models/user.go
@@ -4,9 +4,9 @@ import "gorm.io/gorm"
 
 type User struct {
 	gorm.Model
-	FirstName string `gorm:"uniqueIndex"`
-	LastName  string `gorm:"uniqueIndex"`
-	Email     string `gorm:"not null"`
-	Country   string `gorm:"not null"`
-	Age       int    `gorm:"not null;size:3"`
+	FirstName string `gorm:"uniqueIndex" json:"firstName"`
+	LastName  string `gorm:"uniqueIndex" json:"lastName"`
+	Email     string `gorm:"not null" json:"email"`
+	Country   string `gorm:"not null" json:"country"`
+	Age       int    `gorm:"not null;size:3" json:"age"`
 }
